652f91fb02cc15ea8e4c447abe94213c5b83cc83
Yuchen <tianyuchen0428@gmail.com>
2017-11-14 23:44:28 +0100

    update

--- 014-Longest_Collatz_sequence.js
+++ 014-Longest_Collatz_sequence.js
@@ -6,19 +6,24 @@
 // Using the rule above and starting with 13, we generate the following sequence:
 //
 // 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-// It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+// It can be seen that this sequence (starting at 13 and finishing at 1) contains
+// 10 terms.
+// Although it has not been proved yet (Collatz Problem), it is thought that all
+// starting numbers finish at 1.
 //
 // Which starting number, under one million, produces the longest chain?
 //
 // NOTE: Once the chain starts the terms are allowed to go above one million.
 
-function sequence(n) {
+function sequence(n, CollatzSequence) {
   let count = 1;
   while (n != 1) {
-    if (n % 2 == 0) {
+    console.log(CollatzSequence);
+    if (CollatzSequence.has(n)) {
+      return CollatzSequence[n] + 1;
+    } else if (n % 2 == 0) {
       n = n / 2;
-    }
-    else {
+    } else {
       n = 3 * n + 1;
     }
     ++count;
@@ -30,7 +35,10 @@ function findStartNum() {
   let longestChainStartNum = 0;
   let longestChain = 0;
   for (let n = 1; n < 1000000; ++n) {
-    if(sequence(n) > longestChain) {
+    let CollatzSequence = new Map();
+    let NbTerms = sequence(n, CollatzSequence);
+    CollatzSequence.set(n, NbTerms);
+    if (NbTerms > longestChain) {
       longestChain = sequence(n);
       longestChainStartNumber = n;
     }
