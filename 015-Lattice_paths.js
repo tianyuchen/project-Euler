a11436a8e4121e67a1b9fbdecd29c9c734b68fdf
Yuchen <tianyuchen0428@gmail.com>
2017-11-17 00:09:01 +0100

    add comment

--- 015-Lattice_paths.js
+++ 015-Lattice_paths.js
@@ -7,6 +7,10 @@ function findLatticePaths(gridSize) {
   let paths = 1;
 
   for (let i = 0; i < gridSize; ++i) {
+  // In order to arrive bottom right corner, it should move gridSize + 1 times
+  // right and gridSize + 1 times down. It is the problem of combination.
+  // n = 2 * (gridSize + 1), r = gridSize + 1, solution = Cnr/Crr
+  // Crr is to reduce the repetitive calculation
       paths *= (2 * gridSize) - i;
       paths /= i + 1;
   }
