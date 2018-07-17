fe6c09c20f26174c517d45fac061e573bddeb8ec
tianyuchen <tianyuchen0428@gmail.com>
2018-07-17 23:49:02 +0200

    Exo 22

---
+++ 022-Names_scores.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file
+# containing over five-thousand first names, begin by sorting it into
+# alphabetical order. Then working out the alphabetical value for each name,
+# multiply this value by its alphabetical position in the list to obtain a
+# name score.
+#
+# For example, when the list is sorted into alphabetical order, COLIN, which is
+# worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
+# would obtain a score of 938 × 53 = 49714.
+#
+# What is the total of all the name scores in the file?
+
+def positionInSortNames(name):
+    with open('022_names.txt') as f:
+        content = f.readlines()
+        for line in content:
+            names = line.replace('"', '').split(',')
+    return sorted(names).index(name) + 1
+
+def alphabeticalValue(name):
+    value = 0
+    for character in name.lower():
+        position = ord(character) - 96
+        value += position
+    return value
+
+def nameScore(name):
+    return positionInSortNames(name) * alphabeticalValue(name)
+
+print(nameScore("COLIN"))
