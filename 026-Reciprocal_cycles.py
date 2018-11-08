0c129e86e4b7ddc5df3f20806982c0507c51f8c8
tianyuchen <tianyuchen0428@gmail.com>
2018-11-08 13:48:04 +0100

    add to exo26

---
+++ 026-Reciprocal_cycles.py
@@ -0,0 +1,30 @@
+# A unit fraction contains 1 in the numerator. The decimal representation of the unit
+# fractions with denominators 2 to 10 are given:
+#
+# 1/2	= 	0.5
+# 1/3	= 	0.(3)
+# 1/4	= 	0.25
+# 1/5	= 	0.2
+# 1/6	= 	0.1(6)
+# 1/7	= 	0.(142857)
+# 1/8	= 	0.125
+# 1/9	= 	0.(1)
+# 1/10	= 	0.1
+# Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen
+# that 1/7 has a 6-digit recurring cycle.
+#
+# Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
+# its decimal fraction part.
+
+# Terminating Fractions — The fractions that do not have a repeating cycle.
+# For eg, 1/2 = 0.5 , 1/4 = 0.25 , 1/5 = 0.2 etc.
+
+# Completely Repeating — The fractions where the entire decimal part is repeating.
+# like 1/3 = 1.(3), 1/7 = 1.(142857), 1/9 = 1.(1) etc
+
+# Repeating after a non-repeating part — where repetition starts in the decimal part
+# after a few non-repeating digits. For eg, 1/6 = .1(6) where ‘1’ is non-repeating.
+
+def recurring_cycle_digits(n):
+    res = 1/n
+    res - int(res)
