57705c726cf8b04d82b30e9e606b772825fc832a
tianyuchen <tianyuchen0428@gmail.com>
2018-11-09 21:20:40 +0100

    solution

--- 027-Quadratic_primes.py
+++ 027-Quadratic_primes.py
@@ -7,7 +7,9 @@
 
   n² + n + 41
 
-  It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+  It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39.
+
+  However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
 
   The incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, −79 and 1601, is −126479.
 
@@ -20,3 +22,26 @@
   Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.
 
 '''
+def isPrime(num):
+    if num <= 1:
+        return False
+    elif num % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(num ** .5) + 1):
+            if num % i == 0:
+                return False
+    return True
+
+max = 0
+for a in range(-999, 1000):
+  for b in range(-999, 1000):
+    n = 0
+    num = b
+    while isPrime(num):
+      n += 1
+      num = n * n + a * n + b
+    if n > max:
+      max = n
+      maxTuple = (a, b)
+print(max, maxTuple)
