0f5ec9144fbe846c2eff36411fe99d02488bef46
tianyuchen <tianyuchen0428@gmail.com>
2018-11-11 20:24:32 +0100

    update

--- 027-Quadratic_primes.py
+++ 027-Quadratic_primes.py
@@ -9,9 +9,11 @@
 
   It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39.
 
-  However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41² + 41 + 41 is clearly divisible by 41.
+  However, when n = 40, 402 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly
+  when n = 41, 41² + 41 + 41 is clearly divisible by 41.
 
-  The incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, −79 and 1601, is −126479.
+  The incredible formula  n² − 79n + 1601 was discovered, which produces 80 primes for
+  the consecutive values n = 0 to 79. The product of the coefficients, −79 and 1601, is −126479.
 
   Considering quadratics of the form:
 
@@ -19,13 +21,14 @@
 
   where |n| is the modulus/absolute value of n
   e.g. |11| = 11 and |−4| = 4
-  Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0.
+  Find the product of the coefficients, a and b, for the quadratic expression that
+  produces the maximum number of primes for consecutive values of n, starting with n = 0.
 
 '''
 def isPrime(num):
     if num <= 1:
         return False
-    elif num % 2 == 0:
+    elif num % 2 == 0 and num != 2:
         return False
     else:
         for i in range(3, int(num ** .5) + 1):
@@ -44,8 +47,10 @@ def testEquation(a, b):
     n = 0
     while True:
         num = n ** 2 + a * n + b
-        if isPrime(num): n += 1
-        else: break
+        if isPrime(num):
+          n += 1
+        else:
+          break
     return n
 
 # b has to be prime
@@ -59,5 +64,5 @@ for a in range(-999, 1000):
     if n > max:
       max = n
       max_a = a
-      max_b =b
+      max_b = b
 print(max_a * max_b)
