4f7e3491b7bb585866f10bf1e64b04ad2291e8d6
tianyuchen <tianyuchen0428@gmail.com>
2018-11-12 18:15:19 +0100

    upper bound determination

--- 030-Digit_fifth_powers.py
+++ 030-Digit_fifth_powers.py
@@ -12,5 +12,6 @@ The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 '''
 
-# To determine the upper bound, so the fifth powers of the max single digit is 9^5 = 59049,
-# since it's a 5 digits number 
+# To determine the upper bound, the fifth powers of the max single digit is 9 ^ 5 = 59049,
+# since it's a 5 digits number, so 5 * 9 ^ 5 = 295245,  with 5 digits we can make a 6 digit number.
+# 6*95 = 354294. So 355000 seems like a reasonable upper bound to use.
