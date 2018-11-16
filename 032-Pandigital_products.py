ac83137872e54b13b2af9671f2fa493300f2335f
tianyuchen <tianyuchen0428@gmail.com>
2018-11-16 12:06:05 +0100

    idea

--- 032-Pandigital_products.py
+++ 032-Pandigital_products.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 '''
-We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1
+to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
 
 The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
 
@@ -8,3 +9,10 @@ Find the sum of all products whose multiplicand/multiplier/product identity can 
 
 HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 '''
+
+# Cause we know the number should be from 1 to 9, 9 digits. So it must be
+# double-digit * three-digit = four-digit
+# one-digit * four-digit = four-digit
+
+def is_pandigital():
+    
