e82d033a06aa64c8ac794933e00c321382c9719b
tianyuchen <tianyuchen0428@gmail.com>
2018-11-23 16:00:51 +0100

    solution exo34

--- 034-Digit_factorials.py
+++ 034-Digit_factorials.py
@@ -8,15 +8,20 @@ Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 '''
 
 def factorial(n):
-    if n == 1 or n == 0:
-        return n
+    # factorial of zero is 1
+    if n == 0:
+        return 1
     else:
         return n * factorial(n - 1)
 
-
-for num in range(3, 1000):
+# 9! = 362880 has six digits. So if a number has '9' inside it, in order to have
+# sum of factorials equals the number, the max limit need to be a 6 digit number.
+# 6 * 9! = 2177280 has 7 digits. So the upper limit should be 7 * 9! = 2540160.
+total = 0
+for num in range(3, 2540161):
     sum = 0
     for i in str(num):
         sum += factorial(int(i))
     if num == sum:
-        print(num)
+        total += num
+print(total)
