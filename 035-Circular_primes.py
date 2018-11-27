738e61a0ef2ba1ee77451f9c864276bfca435222
tianyuchen <tianyuchen0428@gmail.com>
2018-11-27 17:32:51 +0100

    solution update

--- 035-Circular_primes.py
+++ 035-Circular_primes.py
@@ -8,33 +8,49 @@ How many circular primes are there below one million?
 '''
 
 def is_prime(n):
-    if n <= 1:
-        return False
-    elif n % 2 == 0:
+    if n == 2:
+        return True
+    elif n % 2 == 0 or n < 2:
         return False
     else:
-        for i in range(3, int(n ** .5) + 1):
+        # only odd numbers
+        for i in range(3, int(n ** .5) + 1, 2):
             if n % i == 0:
                 return False
     return True
 
-print(is_prime(18))
 
-def rotation(N):
-    # ex. N[i:] = [1, 9, 7], [9, 7], [7]  N[:i] = [], [1], [1, 9]
+def rotation(n):
+    N = []
     rot = []
     res = []
+    # from number to list
+    for i in str(n):
+        N.append(i)
+
+    # all rotations of number of separate digits
     for i in range(len(N)):
+        # ex. N[i:] = [1, 9, 7], [9, 7], [7]  N[:i] = [], [1], [1, 9]
         rot.append(N[i:] + N[:i])
+
+    # join separate digits to number
     for num in rot:
         res.append(int(''.join(str(digit) for digit in num)))
     return res
 
-# def circular_primes(n):
-#     for i in range(2, 10 ** 6):
-#         rotation
-#
-#     return rotation(K)
 
+def circular_primes(n):
+    total = 0
+    for i in range(2, 10 ** n):
+        for num in rotation(i):
+            if(is_prime(num)):
+                circular_prime = True
+            else:
+                circular_prime = False
+                break
+                
+        if circular_prime:
+            total += 1
+    return total
 
-print(rotation([1, 9, 7]))
+print(circular_primes(6))
