d63fd2b35fe6d75c4de3f748518b64d9d7d5d2b8
tianyuchen <tianyuchen0428@gmail.com>
2018-11-29 17:16:48 +0100

    fix

--- 034-Digit_factorials.py
+++ 034-Digit_factorials.py
@@ -8,22 +8,18 @@ Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 '''
 import time
 
-def factorial(n):
-    # factorial of zero is 1
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-# 9! = 362880 has six digits. So if a number has '9' inside it, in order to have
-# sum of factorials equals the number, the max limit need to be a 6 digit number.
-# 6 * 9! = 2177280 has 7 digits. So the upper limit should be 7 * 9! = 2540160.
 total = 0
 start = time.time()
 
 # cache the factorials
-factorials = [factorial(i) for i in range(0, 10)]
+factorials = [1]
+for i in range (1, 10):
+    # factorials[-1] return the last element of array
+    factorials.append(factorials[-1] * i)
 
+# 9! = 362880 has six digits. So if a number has '9' inside it, in order to have
+# sum of factorials equals the number, the max limit need to be a 6 digit number.
+# 6 * 9! = 2177280 has 7 digits. So the upper limit should be 7 * 9! = 2540160.
 for num in range(3, 2540161):
     sum = 0
     for i in str(num):
