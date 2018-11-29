7ed6149b4ce322f3ef4f5450ed13ed9551f3343b
tianyuchen <tianyuchen0428@gmail.com>
2018-11-29 16:16:13 +0100

    update solution

--- 034-Digit_factorials.py
+++ 034-Digit_factorials.py
@@ -6,6 +6,7 @@ Find the sum of all numbers which are equal to the sum of the factorial of their
 
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 '''
+import time
 
 def factorial(n):
     # factorial of zero is 1
@@ -18,10 +19,18 @@ def factorial(n):
 # sum of factorials equals the number, the max limit need to be a 6 digit number.
 # 6 * 9! = 2177280 has 7 digits. So the upper limit should be 7 * 9! = 2540160.
 total = 0
+start = time.time()
+
+# cache the factorials
+factorials = [factorial(i) for i in range(0, 10)]
+
 for num in range(3, 2540161):
     sum = 0
     for i in str(num):
-        sum += factorial(int(i))
+        sum += factorials[int(i)]
     if num == sum:
         total += num
-print(total)
+
+elapsed_time = time.time() - start
+
+print "Found %d in %r s." % (total, elapsed_time)
