7a3bd2b8f78f545d3805ac24b306b1ded603f4b4
tianyuchen <tianyuchen0428@gmail.com>
2018-12-03 16:12:20 +0100

    add palindromic function

--- 036-Double_base_palindromes.py
+++ 036-Double_base_palindromes.py
@@ -7,11 +7,15 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 (Please note that the palindromic number, in either base, may not include leading zeros.)
 '''
 
+def is_palindromic(n):
+    n = str(n)
+    return n == n[::-1]
+
 sum = 0
 for i in range(1, 10 ** 6):
     # bin() will convert to binary with format like 0b11101
     binary_i = bin(i)[2:]
 
-    if str(i) == str(i)[::-1] and str(binary_i) == str(binary_i)[::-1]:
+    if is_palindromic(i) and is_palindromic(binary_i):
         sum += i
 print(sum)
