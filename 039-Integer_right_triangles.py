78473d4b3d4fdcfa50eca768119de3d7960327a2
tianyuchen <tianyuchen0428@gmail.com>
2018-12-04 17:57:10 +0100

    idea update

--- 039-Integer_right_triangles.py
+++ 039-Integer_right_triangles.py
@@ -9,3 +9,10 @@ For which value of p ≤ 1000, is the number of solutions maximised?
 '''
 
 # right angle triangle 直角三角形
+# a^2 + b^2 = c^2
+# a + b + c = p <= 1000
+# a + b > c
+
+# from c = p - a - b and  a^2 + b^2 = c^2, we can obtain b = p(2a -p)/2(a-p)
+
+a ** 2 + b ** 2 == c ** 2
