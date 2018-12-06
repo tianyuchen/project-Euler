76a55d44326545b318e993279ca7956a49c05b68
tianyuchen <tianyuchen0428@gmail.com>
2018-12-06 15:14:57 +0100

    solution

--- 039-Integer_right_triangles.py
+++ 039-Integer_right_triangles.py
@@ -14,5 +14,18 @@ For which value of p ≤ 1000, is the number of solutions maximised?
 # a + b > c
 
 # from c = p - a - b and  a^2 + b^2 = c^2, we can obtain b = p(2a -p)/2(a-p)
+# assume that a <= b < c so 3a < p, a < p/3
 
-a ** 2 + b ** 2 == c ** 2
+max_solutions = 0
+max_p = 0
+
+for p in range(2, 1001):
+    nb_solutions = 0
+    for a in range(2, p/3):
+        if (p * (2 * a -p)) % (2 * (a - p)) == 0:
+            nb_solutions += 1
+    if nb_solutions > max_solutions:
+        max_solutions = nb_solutions
+        max_p = p
+
+print(max_p)
