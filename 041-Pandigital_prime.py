1081ceae6680821ed73cdbdd89f8c78d7716d699
tianyuchen <tianyuchen0428@gmail.com>
2018-12-07 14:26:03 +0100

    init

---
+++ 041-Pandigital_prime.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+We shall say that an n-digit number is  if it makes use of all the digits
+1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+What is the largest n-digit pandigital prime that exists?
+'''
+
+def pandigital(n):
+    list_num = [i for i in range(1, n+1)]
+    return
+
+
+
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n % 2 == 0 or n < 2:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+print(pandigital(4))
