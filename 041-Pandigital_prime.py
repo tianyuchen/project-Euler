d63573dee1a46dd4775aacbcb18172c55da2f5cb
tianyuchen <tianyuchen0428@gmail.com>
2018-12-07 16:40:43 +0100

    test

--- 041-Pandigital_prime.py
+++ 041-Pandigital_prime.py
@@ -7,10 +7,11 @@ What is the largest n-digit pandigital prime that exists?
 '''
 
 def pandigital(n):
-    list_num = [i for i in range(1, n+1)]
-    return
+    # list_num = [i for i in range(1, n+1)]
+    # join the permutation lists into single numbers by joining each into a string
+    nums = [''.join(item) for item in itertools.permutations(n)]
 
-
+    print(nums)
 
 
 def is_prime(n):
