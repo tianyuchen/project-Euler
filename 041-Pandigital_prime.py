50be2f05ac82e1405d8dcfa26d18c8e0cf50bf36
tianyuchen <tianyuchen0428@gmail.com>
2018-12-10 18:01:55 +0100

    solution

--- 041-Pandigital_prime.py
+++ 041-Pandigital_prime.py
@@ -7,14 +7,16 @@ What is the largest n-digit pandigital prime that exists?
 '''
 import itertools
 
-def pandigital(n):
-    list_num = ''.join(i) for i in range(1, 4)
-    print(list_num)
-    # join the permutation lists into single numbers by joining each into a string
-    nums = [''.join(item) for item in itertools.permutations(n)]
-    print(nums)
+def pandigital_numbers(n):
+    # Generate list of number 1 to n.  ex: '1234'
+    list_num = ''.join([str(i) for i in range(1, n + 1)])
 
-
+    # List all pandigital numbers of digits 1 to n
+    pandigital_str = [''.join(item) for item in itertools.permutations(list_num)]
+    # transfer pendigital number from string to int
+    pandigital_num = [int(num) for num in pandigital_str]
+    pandigital_num.sort(reverse=True)
+    return pandigital_num
 
 def is_prime(n):
     if n == 2:
@@ -26,4 +28,10 @@ def is_prime(n):
             return False
     return True
 
-print(pandigital('123'))
+def largest_pandigital_prime():
+    for i in range(9, 0, -1):
+        for num in pandigital_numbers(i):
+            if is_prime(num):
+                return num
+
+print(largest_pandigital_prime())
