0d4c1821f18c5f02df4861935c5d786cd5f60231
tianyuchen <tianyuchen0428@gmail.com>
2018-12-14 10:44:24 +0100

    solution

--- 043-Sub_string_divisibility.py
+++ 043-Sub_string_divisibility.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 '''
-The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of
+each of the digits 0 to 9 in some order, but it also has a rather interesting
+sub-string divisibility property.
 
-Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note
+the following:
 
 d2d3d4=406 is divisible by 2
 d3d4d5=063 is divisible by 3
@@ -13,3 +16,14 @@ d7d8d9=728 is divisible by 13
 d8d9d10=289 is divisible by 17
 Find the sum of all 0 to 9 pandigital numbers with this property.
 '''
+
+primes = [2, 3, 5, 7, 11, 13, 17]
+
+
+def is_sub_string_divisible(n):
+    for i in range(1, 8):
+        if int(str(n)[i: i + 3]) % primes[i - 1] != 0:
+            return False
+    return True
+
+print(is_sub_string_divisible(1406357289))
